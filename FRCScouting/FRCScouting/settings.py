6089f32192ffd08838064aa7982b98e11bb8029a
Aaron <aaron@xnovax.net>
2019-06-10 17:30:45 -0400

    Moved some template files

--- FRCScouting/FRCScouting/settings.py
+++ FRCScouting/FRCScouting/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'FRCScouting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['FRCScouting/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
