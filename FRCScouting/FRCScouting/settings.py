d4c486ea0700da96f38e8a458c2fed786bc006a8
Aaron Spindler <nova1104@live.com>
2019-06-13 14:22:23 -0400

    Changed static file directory settings

--- FRCScouting/FRCScouting/settings.py
+++ FRCScouting/FRCScouting/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frcscouting/static/'),
+    os.path.join(BASE_DIR, 'ContentPages/static/'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
