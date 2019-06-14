cde90c4225e657288f0f74647b000e86794e6ae4
Aaron Spindler <nova1104@live.com>
2019-06-14 11:17:01 -0400

    Still trying to fix blog

--- FRCScouting/Blog/views.py
+++ FRCScouting/Blog/views.py
@@ -3,8 +3,8 @@ from .models import Blog
 
 def allblogs(request):
     blogs = Blog.objects
-    return render(request, 'blog/allblogs.html', {'blogs':blogs})
+    return render(request, 'Blog/allblogs.html', {'blogs':blogs})
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html',{'blog':detailblog})
+    return render(request, 'Blog/detail.html',{'blog':detailblog})
--- FRCScouting/FRCScouting/settings.py
+++ FRCScouting/FRCScouting/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'FRCScouting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
