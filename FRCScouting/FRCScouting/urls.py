daa2e64a4dd5d0a65ae845728c59407cc546ac5d
Aaron Spindler <nova1104@live.com>
2019-06-14 11:14:25 -0400

    Trying to fix blog not working in production

--- FRCScouting/FRCScouting/urls.py
+++ FRCScouting/FRCScouting/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('', include('ContentPages.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', include('Blog.urls'), name='blog'),
+    path('blog/', include('Blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
