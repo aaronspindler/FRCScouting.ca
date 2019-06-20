b0ff54d000a0b1486a4372cb72fc9dbb8170049a
Aaron Spindler <nova1104@live.com>
2019-06-20 16:27:21 -0400

    Changed login page to have a link to signup

--- FRCScouting/Scouting/views.py
+++ FRCScouting/Scouting/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url="/account/signup")
+@login_required(login_url="/account/login")
 def dashboard(request):
     return render(request, 'Scouting/Dashboard.html')
