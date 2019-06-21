0fc69ac526e6fd84ef0081955dc0f50380743f21
Aaron Spindler <nova1104@live.com>
2019-06-20 21:10:47 -0400

    Hopefully it's just a capitalization problem

--- FRCScouting/Accounts/views.py
+++ FRCScouting/Accounts/views.py
@@ -8,17 +8,17 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username = request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error':'Username has already been taken!'})
+                return render(request, 'Accounts/signup.html', {'error':'Username has already been taken!'})
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'], first_name=request.POST['firstname'], last_name=request.POST['lastname'], email=request.POST['email'])
                 auth.login(request,user)
                 return redirect('home')
         else:
-            return render(request, 'accounts/signup.html', {'error':'Passwords must match!'})
+            return render(request, 'Accounts/signup.html', {'error':'Passwords must match!'})
 
                 #User wants to enter info
     else:
-        return render(request, 'accounts/signup.html')
+        return render(request, 'Accounts/signup.html')
 
 def login(request):
     if request.method == 'POST':
@@ -27,10 +27,10 @@ def login(request):
             auth.login(request,user)
             return redirect('dashboard')
         else:
-            return render(request, 'accounts/login.html', {'error':'Username/Password is incorrect!'})
+            return render(request, 'Accounts/login.html', {'error':'Username/Password is incorrect!'})
 
     else:
-        return render(request, 'accounts/login.html')
+        return render(request, 'Accounts/login.html')
 
 def logout(request):
     if request.method == 'POST':
