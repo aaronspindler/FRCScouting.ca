b7a66e360f72a5ab1e532f9ed168466eb407076b
Aaron Spindler <nova1104@live.com>
2019-06-25 12:56:15 -0400

    Finished database model for team info, next up is frontend for team info

--- FRCScouting/TheBlueAlliance/utils.py
+++ FRCScouting/TheBlueAlliance/utils.py
@@ -1,15 +1,28 @@
 from FRCScouting import settings
+from .models import Team
+import requests
 
 def get_teaminfo(teamkey):
-    #TODO : Send Request to TBA.com
+    url = 'https://www.thebluealliance.com/api/v3/team/frc{}'.format(teamkey)
+    headers = {'X-TBA-Auth-Key': settings.THE_BLUE_ALLIANCE_KEY}
+    response = requests.get(url, headers=headers)
+    result = response.json()
+    teaminfo = Team()
 
-
-    #TODO : Parse Request
-
-    #TODO : Save to database
-
-    #TODO : return
-    return teamkey
+    teaminfo.number = result['team_number']
+    teaminfo.key = result['key']
+    teaminfo.nickname = result['nickname']
+    teaminfo.name = result['name']
+    teaminfo.city = result['city']
+    teaminfo.state_prov = result['state_prov']
+    teaminfo.country = result['country']
+    teaminfo.address = result['address']
+    teaminfo.postalcode = result['postal_code']
+    teaminfo.website = result['website']
+    teaminfo.rookieyear = result['rookie_year']
+    teaminfo.motto = result['motto']
+    teaminfo.save()
+    return teaminfo
 
 def validate_teamkey(teamkey):
     try:
