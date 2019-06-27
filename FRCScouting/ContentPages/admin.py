97d2bd32a6ac47ee88951e54c3baf71764a11410
Aaron Spindler <nova1104@live.com>
2019-06-27 16:45:30 -0400

    Working on contact page

--- FRCScouting/ContentPages/admin.py
+++ FRCScouting/ContentPages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import GameManual
+from .models import GameManual, ContactMessage
 
 admin.site.register(GameManual)
+admin.site.register(ContactMessage)
--- FRCScouting/ContentPages/models.py
+++ FRCScouting/ContentPages/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 class GameManual(models.Model):
     year = models.CharField(max_length=4)
+    name = models.CharField(max_length=140,null=True)
     file = models.FileField(upload_to='files/')
     image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.year;
+
+class ContactMessage(models.Model):
+    pub_date = models.DateTimeField()
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
