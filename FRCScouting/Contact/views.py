811ebfb43eb02f0dc3aaf1d6b4a0285d6084853d
Aaron Spindler <nova1104@live.com>
2019-06-27 17:56:07 -0400

    Finished contact page

--- FRCScouting/Contact/views.py
+++ FRCScouting/Contact/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import ContactMessage
+from datetime import datetime
 
 def contactus(request):
-    return render(request, 'Contact/contact.html')
+    if request.method == 'POST':
+        contactmessage = ContactMessage()
+        contactmessage.name = request.POST['name']
+        contactmessage.email = request.POST['email']
+        contactmessage.subject = request.POST['subject']
+        contactmessage.message = request.POST['message']
+        contactmessage.sent_date = datetime.now()
+        contactmessage.save()
+        return render(request, 'Contact/contactsuccess.html')
+    else:
+        return render(request, 'Contact/contact.html')
