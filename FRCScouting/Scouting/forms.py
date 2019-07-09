447860766394fc302b9001457809a76a70271ecb
Aaron Spindler <nova1104@live.com>
2019-07-09 14:49:22 -0400

    Got robot image adder working, now need to validate fields

--- FRCScouting/Scouting/forms.py
+++ FRCScouting/Scouting/forms.py
@@ -1,8 +1,1 @@
 from django.forms import ModelForm
-from .models import Robot
-
-class RobotUploadForm(ModelForm):
-    class Meta:
-        model = Robot
-        fields = ['team_number', 'year', 'name', 'image', 'image2', 'image3', 'image4', 'image5']
-        
--- FRCScouting/Scouting/views.py
+++ FRCScouting/Scouting/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import RobotUploadForm
 from .models import Robot
 import datetime
 
@@ -11,28 +10,39 @@ def dashboard(request):
 @login_required(login_url="/account/login")
 def robot_add(request):
     if request.method == 'POST':
-        filled_form = RobotUploadForm(request.POST)
-        if filled_form.is_valid():
-            robot = Robot()
-            robot.team_number = filled_form.cleaned_data['team_number']
-            robot.year = filled_form.cleaned_data['year']
-            robot.name = filled_form.cleaned_data['name']
-            robot.pub_date = datetime.now()
-            robot.submitted_by = request.user
-            if filled_form.cleaned_data['image']:
-                robot.image = filled_form.cleaned_data['image']
-            if filled_form.cleaned_data['image2']:
-                robot.image2 = filled_form.cleaned_data['image2']
-            if filled_form.cleaned_data['image3']:
-                robot.image3 = filled_form.cleaned_data['image3']
-            if filled_form.cleaned_data['image4']:
-                robot.image4 = filled_form.cleaned_data['image4']
-            if filled_form.cleaned_data['image5']:
-                robot.image5 = filled_form.cleaned_data['image5']
-            robot.save()
+        robot = Robot()
+        # TODO: Validate All Fields
+        robot.team_number = request.POST['team_number']
+        robot.year = request.POST['year']
+        robot.name = request.POST['name']
+        robot.pub_date = datetime.datetime.now()
+        robot.submitted_by = request.user
+        try:
+            robot.image = request.FILES['image']
+        except Exception:
+            pass
+
+        try:
+            robot.image2 = request.FILES['image2']
+        except Exception:
+            pass
+
+        try:
+            robot.image3 = request.FILES['image3']
+        except Exception:
+            pass
+
+        try:
+            robot.image4 = request.FILES['image4']
+        except Exception:
+            pass
+
+        try:
+            robot.image5 = request.FILES['image5']
+        except Exception:
+            pass
+
+        robot.save()
         return render(request, 'Scouting/Dashboard.html')
-
-
     else:
-        form = RobotUploadForm()
-        return render(request, 'Scouting/add_robot.html', {'form':form})
+        return render(request, 'Scouting/add_robot.html')
