4a3c3a86504f7aa4b6276738847c4f7a801e8a8e
Aaron Spindler <nova1104@live.com>
2019-07-08 23:27:16 -0400

    Added robot to models, so users can submit pictures of robots

--- FRCScouting/Scouting/admin.py
+++ FRCScouting/Scouting/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+
+from .models import Robot
+
+admin.site.register(Robot)
--- FRCScouting/Scouting/models.py
+++ FRCScouting/Scouting/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from Accounts.models import User
+
+class Robot(models.Model):
+    team_number = models.IntegerField()
+    year = models.IntegerField(null=True)
+    name = models.CharField(max_length=255, null=True)
+    pub_date = models.DateTimeField(null=True)
+    image = models.ImageField(upload_to='images/', null=True)
+    image2 = models.ImageField(upload_to='images/', null=True)
+    image3 = models.ImageField(upload_to='images/', null=True)
+    image4 = models.ImageField(upload_to='images/', null=True)
+    image5 = models.ImageField(upload_to='images/', null=True)
+    is_approved = models.BooleanField(default=False)
+    submitted_by = models.ForeignKey(User, on_delete=models.CASCADE)
