651c79cabd0efda0e935a2788d08275149e40050
Aaron Spindler <nova1104@live.com>
2019-07-12 14:12:48 -0400

    Updated example_script.sh

--- example_script.sh
+++ example_script.sh
@@ -1,9 +1,16 @@
+cd FRCScouting_Project
+rm -r FRCScoutingenv
+
+virtualenv FRCScoutingenv
+cd ..
+
 source FRCScouting_Project/FRCScoutingenv/bin/activate
 rm -r FRCScouting_Project/FRCScouting.ca/FRCScouting/static
 
 cd FRCScouting_Project/FRCScouting.ca
 git pull
 
+pip install git+https://github.com/TBA-API/tba-api-client-python.git
 pip install -r requirements.txt
 
 cd FRCScouting
