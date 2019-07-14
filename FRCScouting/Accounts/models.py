caf2774e5854a7386eceb21e57b68c1f9c1f7d2d
Aaron Spindler <nova1104@live.com>
2019-07-14 16:29:42 -0400

    Fixed migrations and reset DB

--- FRCScouting/Accounts/models.py
+++ FRCScouting/Accounts/models.py
@@ -5,5 +5,3 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     country = CountryField(null=True)
     team_number = models.IntegerField(null=True)
-    class Meta:
-        db_table = 'auth_user'
