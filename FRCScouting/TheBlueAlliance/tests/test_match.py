6be0ffc39845ac031ede2a367f452e773549220b
Aaron <aaron@xnovax.net>
2019-07-14 16:13:32 -0400

    Working on some more match tests

--- FRCScouting/TheBlueAlliance/tests/test_match.py
+++ FRCScouting/TheBlueAlliance/tests/test_match.py
@@ -7,3 +7,6 @@ class EventTestCase(TestCase):
         print("Testing TBA get_match")
         match_2017cc_f1m1 = get_match('2017cc_f1m1')
         self.assertEqual(match_2017cc_f1m1.key, '2017cc_f1m1')
+        
+        match_2019iri_f1m1 = get_match('2019iri_f1m1')
+        print(match_2019iri_f1m1)
